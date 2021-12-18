cc0bb9c159078f8364ef685cbd035db27c2f784c
Jose Jacy <jacyxixi@gmail.com>
2021-12-17 22:56:06 -0300

    atualizado

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,16 +6,16 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   public appPages = [
-    { title: 'Inicial', url: '', icon: 'home' },
+    { title: 'Home', url: '', icon: 'home' },
+    { title: 'Documentos Úteis', url: '/documentos-uteis', icon: 'document' },
+    { title: 'Notícias', url: '/noticias', icon: 'document' },
+    { title: 'Horários', url: '/horario', icon: 'time' },
+    { title: 'Diário', url: '/diario-eletronico', icon: 'book' },
+    { title: 'Institucional', url: '/institucional', icon: 'build' },
     { title: 'Cadastro', url: '/cadastro', icon: 'person-add' },
     { title: 'Contatos', url: '/contatos', icon: 'call' },
     { title: 'Chat', url: '/chat', icon: 'chatbubbles' },
-    { title: 'Documentos Uteis', url: '/documentos-uteis', icon: 'document' },
-    { title: 'Notícias', url: '/noticias', icon: 'document' },
     { title: 'Perguntas', url: '/perguntas', icon: 'help' },
-    { title: 'Horário Atendimento', url: '/horario', icon: 'time' },
-    { title: 'Diário Eletronico', url: '/diario-eletronico', icon: 'book' },
-    { title: 'Institucional', url: '/institucional', icon: 'build' },
     // { title: 'Inbox', url: '/folder/Inbox', icon: 'mail' },
     // { title: 'Outbox', url: '/folder/Outbox', icon: 'paper-plane' },
     // { title: 'Favorites', url: '/folder/Favorites', icon: 'heart' },
