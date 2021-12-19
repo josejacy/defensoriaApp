4445b919de87ea1bdad7e7405a347784270f5955
Jose Jacy <jacyxixi@gmail.com>
2021-12-18 22:50:57 -0300

    add mapas e atualizado cadastro

--- src/app/cadastro/cadastro.page.ts
+++ src/app/cadastro/cadastro.page.ts
@@ -9,36 +9,51 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
   templateUrl: './cadastro.page.html',
   styleUrls: ['./cadastro.page.scss'],
 })
-// export class CadastroPage implements OnInit {
 
-//   constructor(private ApiService: ApiService) { 
-//     this.createData();
-//     // this.deleteData();
-//   }
-
-//   ngOnInit() {
-//   }
-
-//   createData(){
-//     const data: any = {
-//       title: 'jacy',
-//       body: 'teste',
-//       userId: '10'
-//     };
-//     this.ApiService.createData(data).subscribe(data => {
-//       console.log(data);
-//     })
-//   }
-
-// }
 
 export class CadastroPage  {
 
   formCadastro: FormGroup;
 
   mensagens = {
+    tel_celular: [
+      { tipo: 'required', mensagem: 'O campo telefone é obrigatório.' },
+      { tipo: 'minlength', mensagem: 'O telefone deve ter pelo menos 11 caracteres.' },
+    ],
+    cep: [
+      { tipo: 'required', mensagem: 'O campo cep é obrigatório.' },
+      { tipo: 'minlength', mensagem: 'O telefone deve ter pelo menos 8 caracteres.' },
+    ],
     nome: [
       { tipo: 'required', mensagem: 'O campo Nome é obrigatório.' },
+      { tipo: 'minlength', mensagem: 'O nome deve ter pelo menos 3 caracteres.' },
+    ],
+    estado: [
+      { tipo: 'required', mensagem: 'O campo estado é obrigatório.' },
+      { tipo: 'minlength', mensagem: 'deve ter um estado selecionado.' },
+    ],
+    municipio: [
+      { tipo: 'required', mensagem: 'O campo Município é obrigatório.' },
+      { tipo: 'minlength', mensagem: 'O nome deve ter pelo menos 3 caracteres.' },
+    ],
+    bairro: [
+      { tipo: 'required', mensagem: 'O campo Bairro é obrigatório.' },
+      { tipo: 'minlength', mensagem: 'O nome deve ter pelo menos 3 caracteres.' },
+    ],
+    logradouro: [
+      { tipo: 'required', mensagem: 'O campo logradouro é obrigatório.' },
+      { tipo: 'minlength', mensagem: 'O nome deve ter pelo menos 3 caracteres.' },
+    ],
+    numero: [
+      { tipo: 'required', mensagem: 'O campo numero é obrigatório.' },
+      { tipo: 'minlength', mensagem: 'O nome deve ter pelo menos 3 caracteres.' },
+    ],
+    complemento: [
+      { tipo: 'required', mensagem: 'O campo complemento é obrigatório.' },
+      { tipo: 'minlength', mensagem: 'O nome deve ter pelo menos 3 caracteres.' },
+    ],
+    nome_mae: [
+      { tipo: 'required', mensagem: 'O campo Nome da Mãe é obrigatório.' },
       { tipo: 'minlength', mensagem: 'O nome deve ter pelo menos 3 caracteres.' },
     ],
     cpf: [
@@ -64,7 +79,16 @@ export class CadastroPage  {
 
   constructor(private ApiService: ApiService, private formBuilder: FormBuilder) { 
     this.formCadastro = this.formBuilder.group({
+      tel_celular: ['', Validators.compose([Validators.required, Validators.minLength(9)])],
+      cep: ['', Validators.compose([Validators.required, Validators.minLength(8)])],
       nome: ['', Validators.compose([Validators.required, Validators.minLength(3)])],
+      municipio: ['', Validators.compose([Validators.required, Validators.minLength(3)])],
+      bairro: ['', Validators.compose([Validators.required, Validators.minLength(3)])],
+      estado: ['', Validators.compose([Validators.required, Validators.minLength(2)])],
+      logradouro: ['', Validators.compose([Validators.required, Validators.minLength(3)])],
+      numero: ['', Validators.compose([Validators.required, Validators.minLength(3)])],
+      complemento: ['', Validators.compose([Validators.required, Validators.minLength(3)])],
+      nome_mae: ['', Validators.compose([Validators.required, Validators.minLength(3)])],
       cpf: ['', Validators.compose([Validators.required, CpfValidator.cpfValido])],
       email: ['', Validators.compose([Validators.required, Validators.email])],
       senha: ['', Validators.compose([Validators.required, Validators.minLength(6), Validators.maxLength(8)])],
